2858b9e7b2c2ae56c8c8af1320f58920da8fd07f
Mortaza Jabbarpour <mortaza.jabbarpour@gmail.com>
2023-09-25 00:25:00 +0330

    Initialize Redux and RTK Query

---
+++ redux/store/index.ts
@@ -0,0 +1,40 @@
+import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+import { persistReducer, persistStore } from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
+import { bookParadiseApi } from "@/services/bookParadiseApi";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const combinedReducers = combineReducers({
+  [bookParadiseApi.reducerPath]: bookParadiseApi.reducer,
+  // cart: cartSlice,
+});
+
+const persistedReducers = persistReducer(persistConfig, combinedReducers);
+
+export const store = configureStore({
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(bookParadiseApi.middleware),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useTypedDispatch: () => AppDispatch = useDispatch;
+
+setupListeners(store.dispatch);
---
+++ services/bookParadiseApi.ts
@@ -0,0 +1,196 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import {
+  Author,
+  Book,
+  CategoryWithParent,
+  Publisher,
+  Translator,
+} from "@/types";
+
+export const bookParadiseApi = createApi({
+  reducerPath: "bookParadiseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://book-paradise.ir/api/" }),
+  tagTypes: ["books", "authors", "categories", "publishers", "translators"],
+  endpoints: (builder) => ({
+    //***************************Books******************************//
+    getBooks: builder.query<Book[], void>({
+      query: () => "Admin/Books",
+      providesTags: ["books"],
+    }),
+    addBook: builder.mutation({
+      query: (payload) => ({
+        url: "Admin/Books",
+        method: "POST",
+        body: payload,
+        headers: { accept: "*/*" },
+      }),
+      invalidatesTags: ["books"],
+    }),
+    deleteBook: builder.mutation({
+      query: ({ id }) => ({
+        url: `Admin/Books/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["books"],
+    }),
+    //***************************Categories******************************//
+    getCategories: builder.query<CategoryWithParent[], void>({
+      query: () => "Admin/Categories",
+      providesTags: ["categories"],
+    }),
+    addCategory: builder.mutation({
+      query: (payload) => ({
+        url: "Admin/Categories",
+        method: "POST",
+        body: payload,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["categories"],
+    }),
+    editCategory: builder.mutation({
+      query: (payload) => ({
+        url: `Admin/Categories/${payload.id}`,
+        method: "PUT",
+        body: payload,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["categories"],
+    }),
+    deleteCategory: builder.mutation({
+      query: ({ id }) => ({
+        url: `Admin/Categories/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["categories"],
+    }),
+    //***************************Authors******************************//
+    getAuthors: builder.query<Author[], void>({
+      query: () => "Admin/Authors",
+      providesTags: ["authors"],
+    }),
+    addAuthor: builder.mutation({
+      query: (payload) => ({
+        url: "Admin/Authors",
+        method: "POST",
+        body: payload,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["authors"],
+    }),
+    editAuthor: builder.mutation({
+      query: (payload) => ({
+        url: `Admin/Authors/${payload.id}`,
+        method: "PUT",
+        body: payload,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["authors"],
+    }),
+    deleteAuthor: builder.mutation({
+      query: ({ id }) => ({
+        url: `Admin/Authors/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["authors"],
+    }),
+    //***************************Publishers******************************//
+    getPublishers: builder.query<Publisher[], void>({
+      query: () => "Admin/Publisher",
+      providesTags: ["publishers"],
+    }),
+    addPublisher: builder.mutation({
+      query: (payload) => ({
+        url: "Admin/Publisher",
+        method: "POST",
+        body: payload,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["publishers"],
+    }),
+    editPublisher: builder.mutation({
+      query: (payload) => ({
+        url: `Admin/Publisher/${payload.id}`,
+        method: "PUT",
+        body: payload,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["publishers"],
+    }),
+    deletePublisher: builder.mutation({
+      query: ({ id }) => ({
+        url: `Admin/Publisher/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["publishers"],
+    }),
+    //***************************Translators******************************//
+    getTranslators: builder.query<Translator[], void>({
+      query: () => "Admin/Translators",
+      providesTags: ["translators"],
+    }),
+    addTranslator: builder.mutation({
+      query: (payload) => ({
+        url: "Admin/Translators",
+        method: "POST",
+        body: payload,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["translators"],
+    }),
+    editTranslator: builder.mutation({
+      query: (payload) => ({
+        url: `Admin/Translators/${payload.id}`,
+        method: "PUT",
+        body: payload,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["translators"],
+    }),
+    deleteTranslator: builder.mutation({
+      query: ({ id }) => ({
+        url: `Admin/Translators/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["translators"],
+    }),
+  }),
+});
+
+export const {
+  useGetBooksQuery,
+  useGetCategoriesQuery,
+  useGetAuthorsQuery,
+  useGetTranslatorsQuery,
+  useGetPublishersQuery,
+  useAddBookMutation,
+  useAddAuthorMutation,
+  useEditAuthorMutation,
+  useDeleteAuthorMutation,
+  useAddTranslatorMutation,
+  useDeleteTranslatorMutation,
+  useEditTranslatorMutation,
+  useAddPublisherMutation,
+  useDeletePublisherMutation,
+  useEditPublisherMutation,
+  useAddCategoryMutation,
+  useDeleteCategoryMutation,
+  useEditCategoryMutation,
+  useDeleteBookMutation,
+} = bookParadiseApi;
