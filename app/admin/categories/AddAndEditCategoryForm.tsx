4bb6bfe3dd5a911261a9f749a6b4b3502c44bd60
Mortaza Jabbarpour <mortaza.jabbarpour@gmail.com>
2023-09-25 00:37:33 +0330

    Add Admin/Categories route and files

---
+++ app/admin/categories/AddAndEditCategoryForm.tsx
@@ -0,0 +1,123 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { toast } from "react-toastify";
+
+import {
+  useAddCategoryMutation,
+  useEditCategoryMutation,
+  useGetCategoriesQuery,
+} from "@/services/bookParadiseApi";
+
+import { Category } from "@/types";
+
+const categorySchema = z
+  .object({
+    title: z.string().trim().min(3, "وارد کردن عنوان الزامیست (حداقل 3 حرف)"),
+    parentId: z.string().nullable(),
+  })
+  .required();
+
+type CategorySchema = z.infer<typeof categorySchema>;
+
+interface Props {
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedCategory?: Category;
+}
+
+const AddAndEditCategoryForm: React.FC<Props> = ({
+  setIsModalOpen,
+  selectedCategory,
+}) => {
+  const isInEditMode = Boolean(selectedCategory);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CategorySchema>({
+    resolver: zodResolver(categorySchema),
+    defaultValues: {
+      title: selectedCategory?.title,
+    },
+  });
+
+  const { data: categories } = useGetCategoriesQuery();
+
+  if (categories) console.log(categories);
+
+  const [addCategory, addCategoryResponse] = useAddCategoryMutation();
+
+  const [editCategory, editCategoryResponse] = useEditCategoryMutation();
+
+  const onSubmit: SubmitHandler<CategorySchema> = (inputData) => {
+    let modifiedData = { ...inputData };
+
+    if (Number(inputData.parentId) === 0) modifiedData.parentId = null;
+
+    if (isInEditMode) {
+      editCategory({ id: selectedCategory?.id, ...modifiedData })
+        .unwrap()
+        .then((res) => {
+          toast.success(res.message);
+          setIsModalOpen(false);
+          console.log(res);
+        })
+        .then((error) => {
+          console.log(error);
+        });
+    } else {
+      addCategory(modifiedData)
+        .unwrap()
+        .then((res) => {
+          toast.success(res.message);
+          setIsModalOpen(false);
+        })
+        .then((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  return (
+    <>
+      <form
+        className="flex flex-col overflow-y-auto max-h-[60vh] px-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label htmlFor="title">عنوان:</label>
+        <input
+          id="title"
+          className="input"
+          type="text"
+          {...register("title")}
+        />
+        <span className="input-error">{errors?.title?.message}</span>
+
+        <label htmlFor="parentId">دسته‌بندی والد:</label>
+        <select id="parentId" className="input" {...register("parentId")}>
+          <option disabled selected value={0}>
+            -- جهت انتخاب کلیک کنید --
+          </option>
+          {categories?.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.title}
+            </option>
+          ))}
+        </select>
+        <span className="input-error">{errors?.parentId?.message}</span>
+
+        <button
+          type="submit"
+          className="submit-button"
+          disabled={addCategoryResponse.isLoading}
+        >
+          ثبت
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default AddAndEditCategoryForm;
---
+++ app/admin/categories/page.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useMemo, useState } from "react";
+
+import { PencilIcon, TrashIcon } from "@heroicons/react/24/solid";
+
+import { ColDef } from "ag-grid-community";
+
+import Modal from "@/components/Modal";
+import AddButton from "../components/AddButton";
+import Table from "../components/Table";
+import AddAndEditCategoryForm from "./AddAndEditCategoryForm";
+
+import {
+  useDeleteCategoryMutation,
+  useGetCategoriesQuery,
+} from "@/services/bookParadiseApi";
+
+import { Category } from "@/types";
+
+const Categories = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [selectedCategory, setSelectedCategory] = useState<Category>();
+
+  const { data: categories } = useGetCategoriesQuery();
+
+  const [deleteCategory, deleteCategoryResponse] = useDeleteCategoryMutation();
+
+  const columnDefs: ColDef[] = useMemo(
+    () => [
+      { field: "title", headerName: "عنوان" },
+      {
+        field: "parent",
+        headerName: "دسته‌بندی والد",
+        cellRenderer: (params) => params.data.parent?.title || "-",
+      },
+      {
+        cellRenderer: (params) => {
+          return (
+            <div className="ag-table-icon-container">
+              <PencilIcon
+                className="w-4 h-4"
+                onClick={() => {
+                  setSelectedCategory(params.data);
+                  setIsModalOpen(true);
+                }}
+              />
+              <TrashIcon
+                className="w-4 h-4"
+                onClick={() => deleteCategory({ id: params.data.id })}
+              />
+            </div>
+          );
+        },
+        maxWidth: 100,
+      },
+    ],
+    []
+  );
+
+  return (
+    <>
+      <AddButton
+        onClick={() => {
+          setIsModalOpen(true);
+          setSelectedCategory(undefined);
+        }}
+      />
+      <Table rowData={categories} columnDefs={columnDefs} />
+      <Modal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        title="افزودن دسته‌بندی"
+      >
+        <AddAndEditCategoryForm
+          selectedCategory={selectedCategory}
+          setIsModalOpen={setIsModalOpen}
+        />
+      </Modal>
+    </>
+  );
+};
+
+export default Categories;
