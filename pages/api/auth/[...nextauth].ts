19b51718b33c0746aa28a70f870dec9330f43802
Mortaza Jabbarpour <mortaza.jabbarpour@gmail.com>
2023-10-09 23:32:57 +0330

    Activate Refresh Tokens

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,48 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { JWT, NextAuthOptions } from "next-auth";
 
 import CredentialsProvider from "next-auth/providers/credentials";
+
+async function refreshToken(tokenObject: JWT) {
+  try {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const body = JSON.stringify({
+      id: tokenObject.id,
+      userName: tokenObject.userName,
+      email: tokenObject.email,
+      token: tokenObject.token,
+      refreshToken: tokenObject.refreshToken,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers,
+      body,
+    };
+
+    const res = await fetch(
+      process.env.BASE_URL + "Account/RefreshToken",
+      requestOptions
+    );
+
+    const user = await res.json();
+
+    return { ...user, expiresAt: Date.now() + 60 * 1000 };
+  } catch (error) {
+    return {
+      ...tokenObject,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        username: { label: "Email", type: "text" },
+        userName: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
@@ -28,26 +63,31 @@ export const authOptions: NextAuthOptions = {
         };
 
         const res = await fetch(
-          "https://book-paradise.ir/api/Account/Login",
+          process.env.BASE_URL + "Account/Login",
           requestOptions
         );
 
         const user = await res.json();
 
-        if (user) return user;
+        if (user) return { ...user, expiresAt: Date.now() + 60 * 1000 };
         else return null;
       },
     }),
   ],
 
-  session: { strategy: "jwt" },
+  session: { strategy: "jwt", maxAge: 30 * 24 * 60 * 60 },
 
   pages: { signIn: "/login" },
 
-  jwt: { maxAge: 60 * 60 * 24 * 30 },
-
   callbacks: {
     async jwt({ token, user }) {
+      const expiresAt = token.expiresAt as number;
+
+      if (expiresAt && Date.now() > expiresAt) {
+        const newToken = await refreshToken(token);
+        return { ...token, ...newToken };
+      }
+
       return { ...token, ...user };
     },
     async session({ session, token, user }) {
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
@@ -12,6 +13,35 @@ declare module "next-auth" {
       refreshToken: string;
       sub: string;
       userName: string;
+      expiresAt: number;
     };
+  }
+  interface User {
+    id: number;
+    userName: string;
+    email: string;
+    token: string;
+    refreshToken: string;
+    expiresAt: number;
+  }
+
+  interface JWT {
+    id?: number;
+    userName?: string;
+    email?: string;
+    token?: string;
+    refreshToken?: string;
+    expiresAt?: number & DefaultJWT;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id?: number;
+    userName?: string;
+    email?: string;
+    token?: string;
+    refreshToken?: string;
+    expiresAt?: number & DefaultJWT;
   }
 }
